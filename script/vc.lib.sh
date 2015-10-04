b93d73e8748328b5524c89cf864a8deee5bce505
B. van Berkum <dev@dotmpe.com>
2015-10-05 00:51:38 +0200

    -

---
+++ script/vc.lib.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+
+vc_gitdir()
+{
+  test -n "$1" || set -- "."
+  test -d "$1" || err "vc-gitdir expected dir argument" 1
+  test -z "$2" || err "vc-gitdir surplus arguments" 1
+  test -d "$1/.git" && {
+    echo "$1/.git"
+  } || {
+    test "$1" = "." || cd $1
+    git rev-parse --git-dir 2>/dev/null
+  }
+}
+
