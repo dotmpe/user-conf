c5d26bb71bbb8c89f745f7f2a54ef38c41c5d75b
B. van Berkum <berend@brixcrm.nl>
2015-11-26 17:18:36 +0100

    Moved git-diff util func to vc.lib.sh

--- script/vc.lib.sh
+++ script/vc.lib.sh
@@ -8,6 +8,7 @@ vc_gitdir()
   test -n "$1" || set -- "."
   test -d "$1" || err "vc-gitdir expected dir argument" 1
   test -z "$2" || err "vc-gitdir surplus arguments" 1
+
   test -d "$1/.git" && {
     echo "$1/.git"
   } || {
@@ -18,12 +19,30 @@ vc_gitdir()
 
 vc_gitremote()
 {
-  test -n "$1" || set -- "."
+  test -n "$1" || set -- "." "origin"
   test -d "$1" || err "vc-gitremote expected dir argument" 1
   test -n "$2" || err "vc-gitremote expected remote name" 1
   test -z "$3" || err "vc-gitremote surplus arguments" 1
 
   cd "$(vc_gitdir "$1")"
   git config --get remote.$2.url
+}
+
+vc_gitdiff()
+{
+  test -n "$1" || err "vc-gitdiff expected src" 1
+  test -n "$2" || err "vc-gitdiff expected trgt" 1
+  test -z "$3" || err "vc-gitdiff surplus arguments" 1
+  test -n "$GITDIR" || err "vc-gitdiff expected GITDIR env" 1
+  test -d "$GITDIR" || err "vc-gitdiff GITDIR env is not a dir" 1
+
+  target_sha1="$(git hash-object "$2")"
+  co_path="$(cd $GITDIR;git rev-list --objects --all | grep "^$target_sha1" | cut -d ' ' -f 2)"
+  test -n "$co_path" -a "$1" = "$GITDIR/$co_path" && {
+    return 0
+  } || {
+    error "unknown state for path $2"
+    return 1
+  }
 }
 
