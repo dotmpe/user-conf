c250ba822b1c05e5a15d7c3c264c6e1f88beeec3
B. van Berkum <dev@dotmpe.com>
2016-05-19 23:05:09 +0200

    Testing

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -3,16 +3,10 @@
 set -e
 
 
-#exec 3> ./build/test-results.tap
-
-(
-
-./script/user-conf/test.sh || result=$?
-
-) | tee /tmp/123
-
-#./script/user-conf/test.sh  1>&3 || result=$?
-#exec 3<&-
+exec 3> ./build/test-results.tap
+#./script/user-conf/test.sh || result=$?
+./script/user-conf/test.sh  1>&3 || result=$?
+exec 3<&-
 
 echo test.sh=$result
 
