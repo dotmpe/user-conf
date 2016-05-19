3c5cddae3bfb53898d8a5de4808f6c8282f721a6
B. van Berkum <dev@dotmpe.com>
2016-05-19 22:19:22 +0200

    Testing

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+
 tret=
 
 {
 
   ./script/user-conf/test.sh || r=$tret
   echo tret=$tret
-  test -n "$tret" || exit $tret
+  test -z "$tret" || exit $tret
 
 } | tee ./build/test-results.tap
 
