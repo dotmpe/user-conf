b8e29d058bacc5a4fdf11392fd0eb73c1aee7c9a
B. van Berkum <dev@dotmpe.com>
2016-05-19 23:14:08 +0200

    Testing

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -7,7 +7,7 @@ uc_lib=script/user-conf
 
 rm ./build/test-results.tap || printf ""
 
-export $(echo $hostname | tr -s '-' '_')_SKIP=1
+export $(echo $hostname | tr -s '-.' '__')_SKIP=1
 
 mkdir -vp build
 
@@ -15,10 +15,9 @@ exec 3> ./build/test-results.tap
 c_test "$@" 1>&3 || result=$?
 exec 3<&-
 
-echo test.sh=$result
+echo "Test returned '$result'"
 
 cat build/test-results.tap
-
 
 exit $result
 
