38dd7023fe01d9a8995c9ca0a2e7ee89e03040eb
B. van Berkum <dev@dotmpe.com>
2016-05-19 23:12:17 +0200

    Testing

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -2,12 +2,17 @@
 
 set -e
 
+uc_lib=script/user-conf
+. "$uc_lib"/lib.sh
+
+rm ./build/test-results.tap || printf ""
+
+export $(echo $hostname | tr -s '-' '_')_SKIP=1
 
 mkdir -vp build
-rm ./build/test-results.tap || printf ""
+
 exec 3> ./build/test-results.tap
-#./script/user-conf/test.sh || result=$?
-uc_lib=$(pwd)/script/user-conf ./script/user-conf/test.sh  1>&3 || result=$?
+c_test "$@" 1>&3 || result=$?
 exec 3<&-
 
 echo test.sh=$result
