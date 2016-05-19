bb7cdc96e906fdb7458939cf75c0bca4dbf502b9
B. van Berkum <dev@dotmpe.com>
2016-05-19 23:09:23 +0200

    Testing

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -4,6 +4,7 @@ set -e
 
 
 mkdir -vp build
+rm ./build/test-results.tap || printf ""
 exec 3> ./build/test-results.tap
 #./script/user-conf/test.sh || result=$?
 uc_lib=$(pwd)/script/user-conf ./script/user-conf/test.sh  1>&3 || result=$?
