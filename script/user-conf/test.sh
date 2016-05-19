56f293384d45101f60cbf3f6f5b7212f2b60e73a
B. van Berkum <dev@dotmpe.com>
2016-05-19 21:31:55 +0200

    Testing

--- script/user-conf/test.sh
+++ script/user-conf/test.sh
@@ -6,8 +6,11 @@ base=uconf:test
 test -n "$uc_lib" || uc_lib="$(cd "$(dirname "$0")"; pwd)"
 . "$uc_lib"/lib.sh
 export $(echo $hostname | tr -s '-' '_')_SKIP=1
-c_test "$@" || {
-  ret2=$?
-  echo ret2=$ret2
-  exit 1
-}
+c_test "$@"
+
+#|| {
+#  ret2=$?
+#  echo ret2=$ret2
+#  exit 1
+#}
+#
