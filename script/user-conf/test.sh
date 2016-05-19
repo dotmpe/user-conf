e9c1ebbe3983f5e287936392257efbd7d4a31159
B. van Berkum <dev@dotmpe.com>
2016-05-19 22:28:08 +0200

    Testing

--- script/user-conf/test.sh
+++ script/user-conf/test.sh
@@ -5,6 +5,6 @@ test -n "$uc_lib" || uc_lib="$(cd "$(dirname "$0")"; pwd)"
 . "$uc_lib"/lib.sh
 export $(echo $hostname | tr -s '-' '_')_SKIP=1
 echo c_test "$@"
-c_test "$@"
+( c_test "$@" ) || exit $?
 echo c_test=$?
 
