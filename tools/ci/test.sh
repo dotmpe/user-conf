059f4b755d642a0507b77add514a11f913a7479b
B. van Berkum <berend@brixcrm.nl>
2017-03-28 11:45:00 +0200

    Minor change to prevent verbose error

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -7,11 +7,11 @@ hostnameid=
 uc_lib=script/user-conf
 . "$uc_lib"/lib.sh
 
-rm ./build/test-results.tap || printf ""
+mkdir -vp build
+test ! -e ./build/test-results.tap ||
+  rm ./build/test-results.tap
 
 log "Hostname: $(hostname)"
-
-mkdir -vp build
 
 exec 3> ./build/test-results.tap
 c_test "$@" 1>&3 || result=$?
