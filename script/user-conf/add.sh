45618ea7d8a2d624e75250856871d8ffb7d08936
B. van Berkum <dev@dotmpe.com>
2021-01-23 18:21:51 +0100

    Removed old fe scripts

--- script/user-conf/add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-base=uconf:add
-test -n "$uc_lib" || uc_lib="$(cd "$(dirname "$0")"; pwd)"
-. "$uc_lib"/lib.sh
-{ uc__add "$@" || exit $? ; } \
-  2>&1 | ~/.conf/script/uc-colorize.sh
-
