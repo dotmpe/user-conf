d19924179e8c4633a8abc4df730589f78fcc16f9
B. van Berkum <dev@dotmpe.com>
2024-11-19 22:50:49 +0100

    t460s

--- run-dev.sh
+++ run-dev.sh
@@ -5,7 +5,7 @@ docker rm -f user-conf-dev || true
 
 image=dotmpe/treebox:edge
 
-PPWD=$(pwd -P)
+PWP=$(pwd -P)
 gh_keyfile=~/.ssh/id_rsa
 kbn=id_rsa
 set -x
@@ -18,13 +18,13 @@ docker run \
   --volume $(realpath $gh_keyfile):/home/treebox/.ssh/$kbn \
   --volume $(realpath ~/.ssh/known_hosts):/home/treebox/.ssh/known_hosts \
   --volume /srv/project-local/user-scripts:/srv/project-local/user-scripts \
-  --volume $PPWD:$PPWD \
+  --volume $PWP:$PWP \
   -u treebox \
   -e UC_LOG_LEVEL=7 \
   -e USER=treebox \
   -e TERM=xterm-256color \
   -e LOG=$PWD/tool/sh/log.sh \
-  -w $PPWD \
+  -w $PWP \
   $image "$@" </dev/tty
 
 #
